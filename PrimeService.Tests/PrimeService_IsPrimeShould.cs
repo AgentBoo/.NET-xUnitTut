1db38d9e1a33481488746d3ebbafc90e57fedcbb
Zuzka Bi <bongobongo.choo@outlook.com>
2018-08-19 17:48:53 -0400

    Add project files.

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        // Fact attr indicates a test method run by the test runner 
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+        // Theory attr represents a suite of tests with different input args 
+        // InlineData attr specifies values for those inputs 
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void ReturnFalseGivenValuesLessThan2(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// first create a failing implementation of the PrimeService class 
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+    }
+}
